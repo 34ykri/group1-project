fb0e27336d0c32c913b4bba66d690b3d9c8a367e
isaiah-aldiano <iealdiano@gmail.com>
2023-03-29 17:00:17 -0500

    basic java springboot project

---
+++ group1-Music-Project/src/main/java/dmacc/Group1MusicProjectApplication.java
@@ -0,0 +1,13 @@
+package dmacc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group1MusicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group1MusicProjectApplication.class, args);
+	}
+
+}
