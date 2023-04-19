a2c91c8cf05d5e145b711614341df574320cb064
emmac <steph.sis0@gmail.com>
2023-04-18 19:47:34 -0500

    contact us form added

---
+++ group1-Music-Project/src/main/java/dmacc/controller/ContactController.java
@@ -0,0 +1,17 @@
+package dmacc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ContactController {
+	
+	@GetMapping("/")
+	public String contact(Model model) {
+		model.addAttribute("userForm"); 
+		return "ContactUs"; 
+	}
+
+}
