404d24d48ec0e28592ac08f6042555017183723d
Alex <112102138+34ykri@users.noreply.github.com>
2023-04-26 12:40:27 -0500

    user fix

--- group1-Music-Project/src/main/java/dmacc/beans/User.java
+++ group1-Music-Project/src/main/java/dmacc/beans/User.java
@@ -20,7 +20,7 @@ public class User {
 	private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable=false)
     private int id;
 
